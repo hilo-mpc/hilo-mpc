29aeee0bb19fee4819f3be526d018c1eef01e344
pohlodek <johannes.pohlodek@ovgu.de>
2022-04-05 23:09:02 +0200

    Added Object class

--- hilo_mpc/modules/object.py
+++ hilo_mpc/modules/object.py
@@ -20,3 +20,60 @@
 #   You should have received a copy of the GNU Lesser General Public License
 #   along with HILO-MPC. If not, see <http://www.gnu.org/licenses/>.
 #
+
+from typing import Optional
+import uuid
+
+
+class Object:
+    """
+    Defines common behavior of various objects in HILO-MPC.
+
+    :param id: Identifier of the object
+    :type id: str, optional
+    :param name: Name of the object
+    :type name: str, optional
+    """
+    def __init__(self, id: Optional[str] = None, name: Optional[str] = None) -> None:
+        """Constructor method"""
+        self._id = id
+        self.name = name
+
+    def __repr__(self) -> str:
+        """Representation method"""
+        args = ""
+        if self._id is not None:
+            args += f"id='{self._id}'"
+        if self.name is not None:
+            args += f", name='{self.name}'"
+        return f"{self.__class__.__name__}({args})"
+
+    def __str__(self) -> str:
+        """String representation method"""
+        return f"{self.__class__.__name__}: {self.name} ({self._id})"
+
+    def _create_id(self) -> None:
+        """
+        Generates a unique identifier for the object.
+
+        :return:
+        """
+        self._id = str(uuid.uuid4())
+
+    @property
+    def id(self) -> str:
+        """
+        Identifier of the object
+
+        :return: Identifier of the object
+        :rtype: str
+        """
+        return self._id
+
+    @id.setter
+    def id(self, value: str) -> None:
+        if value != self._id:
+            if not isinstance(value, str):
+                raise TypeError(f"{self.__class__.__name__} ID must be a string")
+            else:
+                self._id = value
