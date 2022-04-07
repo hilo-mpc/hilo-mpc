68dbb55e7d5021591285ac83f2ba341f4d00918d
pohlodek <johannes.pohlodek@ovgu.de>
2022-04-08 00:00:21 +0200

    Added Base class and required methods

--- hilo_mpc/modules/base.py
+++ hilo_mpc/modules/base.py
@@ -20,3 +20,372 @@
 #   You should have received a copy of the GNU Lesser General Public License
 #   along with HILO-MPC. If not, see <http://www.gnu.org/licenses/>.
 #
+
+import platform
+from typing import Optional
+import warnings
+
+import casadi as ca
+
+from .object import Object
+from ..util.util import setup_warning, check_compiler, convert, dump_clean, lower_case, AOT, JIT
+
+if platform.system() == 'Linux':
+    from hilo_mpc.util.unix import compile_so
+elif platform.system() == 'Windows':
+    from hilo_mpc.util.windows import compile_dll
+
+
+class Base(Object):
+    """
+    Base class for all building blocks
+
+    :param id: Identifier of the object
+    :type id: str, optional
+    :param name: Name of the object
+    :type name: str, optional
+    :param display: Whether to display certain messages, defaults to True
+    :type display: bool
+    """
+    def __init__(self, id: Optional[str] = None, name: Optional[str] = None, display: bool = True) -> None:
+        """Constructor method"""
+        super().__init__(id=id, name=name)
+        self._function = None
+        self._display = display
+
+        self._c_name = None
+        self._compiler_opts = {}
+        method, compiler, extra = check_compiler('jit', 'clang')
+        if method is not None and compiler is not None:
+            self._compiler_opts['method'] = method
+            self._compiler_opts['compiler'] = compiler
+            if extra is not None:
+                self._compiler_opts['extra'] = extra
+
+    def __call__(self, *args, **kwargs):
+        """Calling method"""
+        # TODO: Add typing information
+        which = kwargs.pop('which', None)
+        if which is None:
+            which = '_function'
+        if not which.startswith('_'):
+            which = '_' + which
+        func = getattr(self, which)
+        return func(*args, **kwargs)
+
+    def __repr__(self) -> str:
+        """Representation method"""
+        args = ""
+        if self._id is not None:
+            args += f"id='{self._id}'"
+        if self.name is not None:
+            args += f", name='{self.name}'"
+        args += f", display={self._display}"
+        return f"{self.__class__.__name__}({args})"
+
+    @setup_warning
+    def _set(self, attr, args, **kwargs):
+        """
+
+        :param attr:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        # TODO: Typing hints
+        if attr not in ['ode', 'alg', 'meas', 'obj', 'cons']:
+            pointer = getattr(self, '_' + attr)
+        else:
+            if hasattr(self, '_rhs'):
+                pointer = self._rhs
+            elif hasattr(self, '_problem'):
+                pointer = self._problem
+            else:
+                pointer = None
+
+        if isinstance(pointer, (ca.SX, ca.MX, ca.DM)):
+            setattr(self, '_' + attr, convert(args, type(pointer), name=attr))
+        else:
+            if type(pointer).__module__ != 'hilo_mpc.modules.base':
+                raise TypeError(f"Wrong type {type(pointer).__name__} for supplied attribute")
+            if isinstance(pointer, Vector):
+                if args is not None:
+                    pointer.set(args, **kwargs)
+                else:
+                    pointer.set(attr, **kwargs)
+            elif isinstance(pointer, RightHandSide):
+                if isinstance(args, (dict, RightHandSide)):
+                    pointer.set(args, **kwargs)
+                else:
+                    pointer.set({attr: args}, **kwargs)
+            elif isinstance(pointer, Problem):
+                if isinstance(args, (dict, Problem)):
+                    pointer.set(args, **kwargs)
+                else:
+                    pointer.set({attr: args}, **kwargs)
+            else:
+                raise TypeError(f"Wrong type '{type(attr).__name__}' for supplied attribute")
+
+    @setup_warning
+    def _add(self, attr, args, position, **kwargs):
+        """
+
+        :param attr:
+        :param args:
+        :param position:
+        :param kwargs:
+        :return:
+        """
+        # TODO: Typing hints
+        if attr not in ['ode', 'alg', 'meas', 'obj', 'cons']:
+            pointer = getattr(self, '_' + attr)
+        else:
+            if hasattr(self, '_rhs'):
+                pointer = self._rhs
+            elif hasattr(self, '_problem'):
+                pointer = self._problem
+            else:
+                pointer = None
+
+        if isinstance(pointer, (ca.SX, ca.MX, ca.DM)):
+            # setattr(self, '_' + attr, convert(args, type(pointer), name=attr))
+            raise NotImplementedError
+        else:
+            if type(pointer).__module__ != 'hilo_mpc.modules.base':
+                raise TypeError(f"Wrong type {type(pointer).__name__} for supplied attribute")
+            if isinstance(pointer, Vector):
+                if args is not None:
+                    pointer.add(args, **kwargs)
+                else:
+                    pointer.add(attr, **kwargs)
+            elif isinstance(pointer, RightHandSide):
+                if isinstance(args, (dict, RightHandSide)):
+                    pointer.add(args, **kwargs)
+                else:
+                    pointer.add({attr: args}, **kwargs)
+            elif isinstance(pointer, Problem):
+                if isinstance(args, (dict, Problem)):
+                    pointer.add(args, **kwargs)
+                else:
+                    pointer.add({attr: args}, **kwargs)
+            else:
+                raise TypeError(f"Wrong type '{type(attr).__name__}' for supplied attribute")
+
+    @setup_warning
+    def _remove(self, attr, args):
+        """
+
+        :param attr:
+        :param args:
+        :return:
+        """
+        # TODO: Typing hints
+        if attr not in ['ode', 'alg', 'meas', 'obj', 'cons']:
+            pointer = getattr(self, '_' + attr)
+        else:
+            if hasattr(self, '_rhs'):
+                pointer = self._rhs
+            elif hasattr(self, '_problem'):
+                pointer = self._problem
+            else:
+                pointer = None
+
+        if isinstance(args, (int, slice)):
+            del pointer[args]
+        elif isinstance(args, (list, tuple)) and all(isinstance(arg, int) for arg in args):
+            # TODO: Do we need to loop in reversed order?
+            for arg in args:
+                del pointer[arg]
+        else:
+            pointer.remove(args)
+
+    def _generator(self, **kwargs):
+        """
+
+        :param kwargs:
+        :return:
+        """
+        generator = kwargs.get('generator')
+        if generator is not None:
+            if not isinstance(generator, dict):
+                gen_path = None
+                gen_name = None
+                gen_opts = None
+            else:
+                gen_path = generator.get('path')
+                gen_name = generator.get('name')
+                gen_opts = generator.get('opts')
+        else:
+            gen_path = kwargs.get('gen_path')
+            gen_name = kwargs.get('gen_name')
+            gen_opts = kwargs.get('gen_opts')
+
+        if gen_path is None:
+            session = kwargs.get('session')
+            if session is not None:
+                gen_path = session.path
+
+        if gen_name is None:
+            if self.name is None:
+                gen_name = lower_case(self.__class__.__name__)
+            else:
+                gen_name = self.name.replace(' ', '_')
+
+        if gen_opts is not None:
+            if platform.system() == 'Linux':
+                if 'cpp' in gen_opts:
+                    if gen_opts['cpp'] and self._compiler_opts['compiler'] == 'gcc':
+                        warnings.warn("The option 'cpp' for the CodeGenerator can not be set to true, when the compiler"
+                                      " 'gcc' is set. Please use a compiler that is able to compile C++ code, "
+                                      "e.g., 'g++'. Setting option 'cpp' to False.")
+                        gen_opts['cpp'] = False
+                    elif not gen_opts['cpp'] and self._compiler_opts['compiler'] == 'g++':
+                        warnings.warn("The option 'cpp' for the CodeGenerator can not be set to false, when the "
+                                      "compiler 'g++' is set. Please use a compiler that is able to compile C code, "
+                                      "e.g., 'gcc'. Setting option 'cpp' to True.")
+                        gen_opts['cpp'] = True
+                else:
+                    if self._compiler_opts['compiler'] == 'g++':
+                        warnings.warn("The option 'cpp' for the CodeGenerator is not provided, but the compiler 'g++' "
+                                      "is set. Please use a compiler that is able to compile C code, e.g., 'gcc'. "
+                                      "Setting option 'cpp' to True.")
+                        gen_opts['cpp'] = True
+
+        return gen_path, gen_name, gen_opts
+
+    @property
+    def compiler(self) -> str:
+        """
+
+        :return:
+        """
+        return dump_clean(self._compiler_opts)
+
+    # @compiler.setter
+    def set_compiler(self, method: str, compiler: str) -> None:
+        """
+
+        :param method:
+        :param compiler:
+        :return:
+        """
+        old_method = method
+        old_compiler = compiler
+        method, compiler, extra = check_compiler(method, compiler)
+        if method != self._compiler_opts['method'] or compiler != self._compiler_opts['compiler']:
+            if method is not None and compiler is not None:
+                self._compiler_opts['method'] = method
+                self._compiler_opts['compiler'] = compiler
+                if extra is not None:
+                    self._compiler_opts['extra'] = extra
+            else:
+                warnings.warn(f"Compiler '{old_compiler}' could not be set for method '{old_method}'. No changes "
+                              f"applied.")
+
+    @property
+    def display(self) -> bool:
+        """
+
+        :return:
+        """
+        return self._display
+
+    @display.setter
+    def display(self, value: bool) -> None:
+        if value is not self._display:
+            self._display = value
+
+    def check_consistency(self) -> None:
+        """
+
+        :return:
+        """
+        if self._function is not None:
+            if self._function.has_free():
+                free_vars = ", ".join(self._function.get_free())
+                msg = f"Instance {self.__class__.__name__} has the following free variables/parameters: {free_vars}"
+                raise RuntimeError(msg)
+
+    def setup(self, **kwargs):
+        """
+
+        :param kwargs:
+        :return:
+        """
+        # TODO: Typing hints
+        # TODO: Support for user-defined output
+        if self._compiler_opts['method'] in JIT:
+            importer_opts = {}
+            if self._compiler_opts['compiler'] == 'shell':
+                path = self._compiler_opts.pop('path')
+                # TODO: Cleanup should be True if a user-defined path is given
+                # Cleanup set to false, to prevent warning messages that temporary files are not found. Since closing
+                # the session will remove the temporary path, CasADi cannot find the temporary files anymore.
+                importer_opts['cleanup'] = False
+                importer_opts['name'] = path + 'tmp_casadi_compiler_shell'
+                if platform.system() == 'Windows':
+                    vcvars = self._compiler_opts.get('extra', None)
+                    if vcvars is not None:
+                        compiler = ' && '.join((vcvars, 'cl.exe'))
+                        linker = ' && '.join((vcvars, 'link.exe'))
+                        importer_opts['compiler'] = compiler
+                        importer_opts['linker'] = linker
+                    else:
+                        warnings.warn("Could not find a compiler. Make sure Microsoft Visual Studio is installed "
+                                      "(other compilers are not yet supported).")
+                        return
+            C = ca.Importer(self._c_name, self._compiler_opts['compiler'], importer_opts)
+            self._function = ca.external(self._function, C)
+        elif self._compiler_opts['method'] in AOT:
+            if platform.system() == 'Linux':
+                library = compile_so(self._c_name, self._compiler_opts['compiler'])
+            elif platform.system() == 'Windows':
+                library = compile_dll(self._c_name)
+            else:
+                library = None
+            if library is not None:
+                self._function = ca.external(self._function, library)
+        else:
+            warnings.warn("Unknown method for C/C++ code generation")
+
+    def is_setup(self) -> bool:
+        """
+
+        :return:
+        """
+        if self._function is not None:
+            return True
+        else:
+            return False
+
+
+class Container(Object):
+    """"""
+
+
+class Vector(Container):
+    """"""
+
+
+class Equations:
+    """"""
+
+
+class RightHandSide(Equations):
+    """"""
+
+
+class Problem(Equations):
+    """"""
+
+
+class Series(Object):
+    """"""
+
+
+class TimeSeries(Series):
+    """"""
+
+
+class OptimizationSeries(Series):
+    """"""
--- hilo_mpc/util/util.py
+++ hilo_mpc/util/util.py
@@ -20,3 +20,309 @@
 #   You should have received a copy of the GNU Lesser General Public License
 #   along with HILO-MPC. If not, see <http://www.gnu.org/licenses/>.
 #
+
+from collections import ValuesView
+import functools
+import platform
+import sys
+from typing import Any, Callable, Optional, TypeVar, cast
+import warnings
+
+import casadi as ca
+import numpy as np
+
+if platform.system() == 'Windows':
+    from .windows import get_vcvars, WINDOWS_COMPILERS
+elif platform.system() == 'Linux':
+    from .unix import find_compiler, UNIX_COMPILERS
+
+
+Function = TypeVar('Function', bound=Callable[..., Any])
+
+
+JIT = ['jit', 'just-in-time']
+AOT = ['aot', 'ahead-of-time']
+TYPES = {
+    ca.SX: ca.SX,
+    ca.MX: ca.MX,
+    ca.DM: ca.DM,
+    np.ndarray: np.array
+}
+
+
+def setup_warning(function: Function) -> Function:
+    """
+
+    :param function:
+    :return:
+    """
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        """
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        if args[0].is_setup():
+            warnings.warn(f"{args[0].__class__.__name__} {args[0].name} was already set up. Please run the setup "
+                          f"method again to apply the changes and prevent strange behavior.")
+        return function(*args, **kwargs)
+    return cast(Function, wrapper)
+
+
+def _get_shape(**kwargs):
+    """
+
+    :param kwargs:
+    :return:
+    """
+    n_dim = kwargs.get('n_dim', None)
+    shape = kwargs.get('shape', None)
+    if n_dim is None and shape is None:
+        shape = (1, 1)
+    elif n_dim is not None:
+        if shape is None:
+            axis = kwargs.get('axis', None)
+            if axis is None or axis == 0:
+                shape = (n_dim, 1)
+            elif axis == 1:
+                shape = (1, n_dim)
+            elif axis > 1:
+                raise IndexError("Argument 'axis' is out of bounds for array of dimension 2")
+            else:
+                raise TypeError("Expected argument 'axis' of type {}, got {} instead".format(type(int), type(axis)))
+        else:
+            if isinstance(shape, (int, tuple)):
+                if isinstance(shape, int):
+                    axis = kwargs.get('axis', None)
+                    if axis is None or axis == 0:
+                        shape = (shape, 1)
+                    elif axis == 1:
+                        shape = (1, shape)
+                    elif axis > 1:
+                        raise IndexError("Argument 'axis' is out of bounds for array of dimension 2")
+                    else:
+                        raise TypeError("Expected argument 'axis' of type {}, got {} instead".format(type(int),
+                                                                                                     type(axis)))
+                elif isinstance(shape, tuple):
+                    if len(shape) > 2:
+                        raise IndexError("Argument 'shape' is out of bounds for array of dimension 2")
+                warnings.warn("Argument 'ndim' ignored, since argument 'shape' was supplied.")
+            else:
+                raise TypeError("Expected argument 'shape' of type {} or {}, got {} instead"
+                                .format(type(int), type(tuple), type(shape)))
+
+    return shape
+
+
+def check_compiler(method: str, compiler: str) -> (Optional[str], Optional[str], Optional[str]):
+    """
+
+    :param method:
+    :param compiler:
+    :return:
+    """
+    method = lower_case(method)
+    compiler = lower_case(compiler)
+    if method in JIT:
+        if ca.Importer.has_plugin(compiler):
+            if compiler == 'shell' and platform.system() == 'Windows':
+                vcvars = get_vcvars()
+                return method, compiler, vcvars
+            else:
+                return method, compiler, None
+    elif method in AOT:
+        if platform.system() == 'Linux' and compiler in UNIX_COMPILERS:
+            if find_compiler(compiler):
+                return method, compiler, None
+        elif platform.system() == 'Windows' and compiler in WINDOWS_COMPILERS:
+            vcvars = get_vcvars()
+            if vcvars:
+                return method, compiler, vcvars
+    return None, None, None
+
+
+def convert(obj, _type, **kwargs):
+    """
+
+    :param obj:
+    :param _type:
+    :param kwargs:
+    :return:
+    """
+    # TODO: Convoluted. Simplify if possible.
+    # TODO: Add support for dict?
+    # TODO: Is there a nice way to convert SX to MX and vice versa?
+    fx = TYPES[_type]
+    if obj is None:
+        return convert(fx(), _type, **kwargs)
+    if isinstance(obj, _type):
+        shape = _get_shape(**kwargs)
+        # TODO: What if size is to be reduced to (1, 1)? Should throw an error. Would do nothing here.
+        if shape != (1, 1):
+            if _type in [ca.SX, ca.MX, ca.DM]:
+                if obj.is_scalar():
+                    return ca.repmat(obj, shape)
+                elif obj.is_column() or obj.is_row():
+                    if max(obj.shape) != max(shape):
+                        raise IndexError("Dimension mismatch. Cannot reduce or increase size of the shape.")
+                    if obj.shape == shape[::-1]:
+                        return obj.T
+                    else:
+                        raise Exception("Don't know how I got here. Please inform the maintainer.")
+                elif obj.is_empty():
+                    return obj.reshape(shape)
+            else:
+                if obj.size == 1:
+                    return np.tile(obj, shape)
+                elif 1 in obj.shape:
+                    if max(obj.shape) != max(shape):
+                        raise IndexError("Dimension mismatch. Cannot reduce or increase size of the shape.")
+                    if obj.shape == shape[::-1]:
+                        return obj.T
+                    else:
+                        raise Exception("Don't know how I got here. Please inform the maintainer.")
+        return obj
+    elif isinstance(obj, (list, tuple, np.ndarray, ca.DM)):
+        # TODO: Add support for axis argument
+        if isinstance(obj, (np.ndarray, ca.DM)):
+            return convert(fx(obj), _type, **kwargs)
+        elif not obj:
+            return convert(fx(), _type, **kwargs)
+        else:
+            types = [isinstance(k, (_type, str, int, float)) for k in obj]
+            not_supported = [k for k, val in enumerate(types) if not val]
+            for k in not_supported:
+                if hasattr(obj[k], 'values'):
+                    obj[k] = obj[k].values
+                    types[k] = True
+            if all(types):
+                if all(isinstance(k, _type) for k in obj):
+                    return convert(ca.vertcat(*obj), _type, **kwargs)
+                elif all(isinstance(k, str) for k in obj):
+                    return convert([fx.sym(parse(k)) for k in obj], _type, **kwargs)
+                elif all(isinstance(k, float) for k in obj):
+                    return convert(fx(obj), _type, **kwargs)
+                elif all(isinstance(k, int) for k in obj):
+                    if isinstance(obj, tuple):
+                        if len(obj) > 2:
+                            raise IndexError("Shape is out of bounds for array of dimension 2")
+                    elif isinstance(obj, list):
+                        return convert(fx(obj), _type, **kwargs)
+                    else:
+                        raise TypeError("Expected argument of type {}, got {} instead".format(type(tuple), type(obj)))
+                    name = kwargs.get('name', None)
+                    if name is not None:
+                        if isinstance(name, str):
+                            return convert(fx.sym(name, obj), _type, **kwargs)
+                        else:
+                            raise TypeError("Expected argument 'name' of type {}, got {} instead"
+                                            .format(type(str), type(name)))
+                    else:
+                        raise KeyError("Additional argument 'name' is missing")
+                else:
+                    # Mix of strings and SX/MX and possibly floats
+                    res = []
+                    for k in obj:
+                        if isinstance(k, _type):
+                            res.append(k)
+                        else:
+                            if isinstance(k, str):
+                                res.append(fx.sym(k))
+                            elif isinstance(k, (int, float)):
+                                res.append(fx(k))
+                            else:
+                                raise TypeError(f"Wrong type of arguments for supplied list: {type(k).__name__}")
+                    return convert(res, _type, **kwargs)
+            else:
+                raise TypeError("Wrong type of arguments for function {}".format(whoami()))
+    elif isinstance(obj, str):
+        shape = _get_shape(**kwargs)
+        return fx.sym(parse(obj), shape)
+    elif isinstance(obj, int):
+        if _type in [ca.DM, np.ndarray]:
+            return convert(fx(obj), _type, **kwargs)
+        else:
+            axis = kwargs.get('axis', None)
+            if axis is None or axis == 0:
+                shape = (obj, 1)
+            elif axis == 1:
+                shape = (1, obj)
+            elif axis > 1:
+                raise IndexError("Argument 'axis' is out of bounds for array of dimension 2")
+            else:
+                raise TypeError("Expected argument 'axis' of type {}, got {} instead".format(type(int), type(axis)))
+            name = kwargs.get('name', None)
+            if name is not None:
+                if isinstance(name, str):
+                    return fx.sym(name, shape)
+                else:
+                    raise TypeError("Expected argument 'name' of type {}, got {} instead".format(type(str), type(name)))
+            else:
+                raise KeyError("Additional argument 'name' is missing")
+    elif isinstance(obj, float):
+        return convert(fx(obj), _type, **kwargs)
+    elif isinstance(obj, ValuesView):
+        return convert(list(obj), _type, **kwargs)
+    else:
+        raise TypeError("Wrong type of arguments for function {}".format(whoami()))
+
+
+def dump_clean(obj):
+    """
+
+    :param obj:
+    :return:
+    """
+    # FIXME: Strings also have the attribute __iter__
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print(k)
+                dump_clean(v)
+            else:
+                print("{} : {}".format(k, v))
+    elif isinstance(obj, list):
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dump_clean(v)
+            else:
+                print(v)
+    else:
+        print(obj)
+
+
+def lower_case(obj: Any) -> Any:
+    """
+
+    :param obj:
+    :return:
+    """
+    if isinstance(obj, dict):
+        return {key.lower(): lower_case(val) for key, val in obj.items()}
+    elif isinstance(obj, (list, set, tuple)):
+        return type(obj)(lower_case(k) for k in obj)
+    elif isinstance(obj, str):
+        return obj.lower()
+    else:
+        return obj
+
+
+def parse(string):
+    """
+
+    :param string:
+    :return:
+    """
+    # TODO: Parse string, e.g. if equations are given in strings (could be useful later for writing equations in GUI)
+    # Check if any of the strings in 'OPERATORS' is in 'string'
+    return string
+
+
+def whoami():
+    """
+
+    :return:
+    """
+    return sys._getframe(1).f_code.co_name
