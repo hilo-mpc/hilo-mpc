1b4e3aa127d0e47dfe2f145ff1ef4c8633c646e8
Johannes <johannes.pohlodek@ovgu.de>
2022-05-04 09:31:21 +0200

    Update hilo_mpc/modules/controller/pid.py
    Added support for decoupled multi-variable PID controllers

--- hilo_mpc/modules/controller/pid.py
+++ hilo_mpc/modules/controller/pid.py
@@ -21,27 +21,26 @@
 #   along with HILO-MPC. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from typing import Optional, Union
+from typing import Optional, Sequence, Union
 
 import casadi as ca
 import numpy as np
 
 from .base import Controller
 from ..base import Base, TimeSeries
-from ...util.util import convert
+from ...util.util import convert, is_diagonal
 
 
 Numeric = Union[int, float]
+Array = Union[Sequence[Numeric], np.ndarray]
 
 
 class PID(Controller, Base):
     """
     Class for PID controller
 
-    :param n_inputs:
-    :type n_inputs: int
-    :param n_outputs:
-    :type n_outputs: int
+    :param n_set_points:
+    :type n_set_points: int
     :param id:
     :type id: str, optional
     :param name:
@@ -61,13 +60,12 @@ class PID(Controller, Base):
     """
     def __init__(
             self,
-            n_inputs: int,
-            n_outputs: int,
+            n_set_points: int = 1,
             id: Optional[str] = None,
             name: Optional[str] = None,
-            k_p: Union[Numeric, np.ndarray] = 1.,
-            t_i: Union[Numeric, np.ndarray] = np.inf,
-            t_d: Union[Numeric, np.ndarray] = 0.,
+            k_p: Optional[Union[Numeric, Array]] = None,
+            t_i: Optional[Union[Numeric, Array]] = None,
+            t_d: Optional[Union[Numeric, Array]] = None,
             proportional_on_process_value: bool = False,
             derivative_on_process_value: bool = False,
             plot_backend: Optional[str] = None
@@ -77,16 +75,21 @@ class PID(Controller, Base):
         if self._id is None:
             self._create_id()
 
-        self._n_inputs = n_inputs
-        self._n_outputs = n_outputs
+        self._n_set_points = n_set_points
+        if k_p is None:
+            k_p = self._n_set_points * [1.]
         self._set_tuning_parameter('k_p', k_p)
+        if t_i is None:
+            t_i = self._n_set_points * [np.inf]
         self._set_tuning_parameter('t_i', t_i)
+        if t_d is None:
+            t_d = self._n_set_points * [0.]
         self._set_tuning_parameter('t_d', t_d)
         self._proportional_on_process_value = proportional_on_process_value
         self._derivative_on_process_value = derivative_on_process_value
         self._p_band = False
         self._anti_windup = None
-        self._set_point = ca.DM.zeros(n_inputs)
+        self._set_point = ca.DM.zeros(self._n_set_points)
 
         self._solution = TimeSeries(plot_backend, parent=self)
 
@@ -97,7 +100,7 @@ class PID(Controller, Base):
         """
         self._type = 'PID'
 
-    def _set_tuning_parameter(self, which: str, value: Union[Numeric, np.ndarray]) -> None:
+    def _set_tuning_parameter(self, which: str, value: Union[Numeric, Array]) -> None:
         """
 
         :param which:
@@ -105,12 +108,17 @@ class PID(Controller, Base):
         :return:
         """
         # TODO: Dimensions
+        value = convert(value, np.ndarray, shape=(self._n_set_points, self._n_set_points))
+        if not is_diagonal(value):
+            raise ValueError(f"The number of set points is greater than 1, but the supplied matrix for "
+                             f"{dict({'k_p': 'K_P', 't_i': 'T_I', 't_d': 'T_D'})[which]} is not a diagonal matrix. "
+                             f"Coupled multi-variable control is not supported at the moment.")
         if which == 'k_p':
-            self._k_p = value
+            self._k_p = np.diag(value)
         elif which == 't_i':
-            self._t_i = value
+            self._t_i = np.diag(value)
         elif which == 't_d':
-            self._t_d = value
+            self._t_d = np.diag(value)
 
     def _append_process_value(self, process_value: ca.DM) -> None:
         """
@@ -119,12 +127,12 @@ class PID(Controller, Base):
         :return:
         """
         if self._solution.get_by_id('x').is_empty():
-            process_value_vector = ca.DM.zeros(3, self._n_inputs)
+            process_value_vector = ca.DM.zeros(self._n_set_points, 3)
         else:
-            process_value_vector = ca.reshape(self._solution.get_by_id('x:f'), 3, self._n_inputs)
-            process_value_vector[:-1, :] = process_value_vector[1:, :]
-        process_value_vector[-1, :] = process_value
-        self._solution.add('x', process_value_vector[:])
+            process_value_vector = ca.reshape(self._solution.get_by_id('x:f'), self._n_set_points, 3)
+            process_value_vector[:, :-1] = process_value_vector[:, 1:]
+        process_value_vector[:, -1] = process_value
+        self._solution.add('x', process_value_vector.reshape((3 * self._n_set_points, -1)))
 
     def _append_set_point(self) -> None:
         """
@@ -132,13 +140,13 @@ class PID(Controller, Base):
         :return:
         """
         if self._solution.get_by_id('p').is_empty():
-            set_point_vector = ca.DM.zeros(3, self._n_inputs)
+            set_point_vector = ca.DM.zeros(self._n_set_points, 3)
         else:
             params = self._solution.get_by_id('p:f')
-            set_point_vector = ca.reshape(params[:3 * self._n_inputs], 3, self._n_inputs)
-            set_point_vector[:-1, :] = set_point_vector[1:, :]
-        set_point_vector[-1, :] = self._set_point
-        params = ca.vertcat(set_point_vector[:], self._k_p, self._t_i, self._t_d)
+            set_point_vector = ca.reshape(params[:3 * self._n_set_points], self._n_set_points, 3)
+            set_point_vector[:, :-1] = set_point_vector[:, 1:]
+        set_point_vector[:, -1] = self._set_point
+        params = ca.vertcat(set_point_vector.reshape((3 * self._n_set_points, -1)), self._k_p, self._t_i, self._t_d)
         self._solution.add('p', params)
 
     def _initialize_controller_output(self) -> None:
@@ -146,68 +154,68 @@ class PID(Controller, Base):
 
         :return:
         """
-        u = ca.DM.zeros(self._n_outputs)
+        u = ca.DM.zeros(self._n_set_points)
         self._solution.set('u', u)
 
     @property
-    def proportional_gain(self) -> Numeric:
+    def proportional_gain(self) -> np.ndarray:
         """
 
         :return:
         """
-        return self._k_p
+        return np.diag(self._k_p)
 
     @proportional_gain.setter
-    def proportional_gain(self, k_p: Numeric) -> None:
-        self._k_p = k_p
+    def proportional_gain(self, k_p: Union[Numeric, np.ndarray]) -> None:
+        self._set_tuning_parameter('k_p', k_p)
 
     k_p = proportional_gain
 
     @property
-    def integral_time(self) -> Numeric:
+    def integral_time(self) -> np.ndarray:
         """
 
         :return:
         """
-        return self._t_i
+        return np.diag(self._t_i)
 
     @integral_time.setter
-    def integral_time(self, t_i: Numeric) -> None:
-        self._t_i = t_i
+    def integral_time(self, t_i: Union[Numeric, np.ndarray]) -> None:
+        self._set_tuning_parameter('t_i', t_i)
 
     t_i = integral_time
 
     @property
-    def integral_gain(self) -> Numeric:
+    def integral_gain(self) -> np.ndarray:
         """
 
         :return:
         """
-        return self._k_p / self._t_i
+        return np.diag(self._k_p / self._t_i)
 
     k_i = integral_gain
 
     @property
-    def derivative_time(self) -> Numeric:
+    def derivative_time(self) -> np.ndarray:
         """
 
         :return:
         """
-        return self._t_d
+        return np.diag(self._t_d)
 
     @derivative_time.setter
-    def derivative_time(self, t_d: Numeric) -> None:
-        self._t_d = t_d
+    def derivative_time(self, t_d: Union[Numeric, np.ndarray]) -> None:
+        self._set_tuning_parameter('t_d', t_d)
 
     t_d = derivative_time
 
     @property
-    def derivative_gain(self) -> Numeric:
+    def derivative_gain(self) -> np.ndarray:
         """
 
         :return:
         """
-        return self._k_p * self._t_d
+        return np.diag(self._k_p * self._t_d)
 
     k_d = derivative_gain
 
@@ -221,14 +229,14 @@ class PID(Controller, Base):
 
     @set_point.setter
     def set_point(self, set_point: Union[Numeric, np.ndarray]) -> None:
-        set_point = convert(set_point, ca.DM, axis=1)
-        if self._n_inputs > 1:
-            if set_point.size2() == 1:
-                set_point = ca.repmat(set_point, 1, self._n_inputs)
-            elif set_point.size2() != self._n_inputs:
+        set_point = convert(set_point, ca.DM)
+        if self._n_set_points > 1:
+            if set_point.size1() == 1:
+                set_point = ca.repmat(set_point, self._n_set_points, 1)
+            elif set_point.size1() != self._n_set_points:
                 raise ValueError(f"Dimension mismatch. Supplied dimension for the set point is "
                                  f"{set_point.size1()}x{set_point.size2()}, but required dimension is "
-                                 f"{self._n_inputs}x1.")
+                                 f"{self._n_set_points}x1.")
         self._set_point = set_point
 
     s_p = set_point
@@ -243,40 +251,46 @@ class PID(Controller, Base):
         if dt is None:
             dt = 1.
 
-        process_value = ca.SX.sym('pv', 3, self._n_inputs)
-        set_point = ca.SX.sym('sp', 3, self._n_inputs)
+        process_value = ca.SX.sym('pv', self._n_set_points, 3)
+        set_point = ca.SX.sym('sp', self._n_set_points, 3)
         error = set_point - process_value
-        output = ca.SX.sym('u', self._n_outputs)
+        output = ca.SX.sym('u', self._n_set_points)
         delta_output = 0.
 
         # TODO: Dimensions
-        k_p = ca.SX.sym('k_p')
-        t_i = ca.SX.sym('t_i')
-        t_d = ca.SX.sym('t_d')
+        k_p = ca.SX.sym('k_p', self._n_set_points)
+        t_i = ca.SX.sym('t_i', self._n_set_points)
+        t_d = ca.SX.sym('t_d', self._n_set_points)
+
+        k_p_matrix = ca.diag(k_p)
+        t_i_matrix_inv = ca.diag(1. / t_i)
+        t_d_matrix = ca.diag(t_d)
 
         if self._p_band:
             raise NotImplementedError("The use of the proportional band has not been implemented yet.")
         else:
             if self._proportional_on_process_value:
-                delta_output -= process_value[-1, :] - process_value[-2, :]
+                delta_output -= process_value[:, -1] - process_value[:, -2]
             else:
-                delta_output += error[-1, :] - error[-2, :]
+                delta_output += error[:, -1] - error[:, -2]
 
-        delta_output += dt / t_i * error[-1, :]
+        delta_output += dt * t_i_matrix_inv @ error[:, -1]
         # TODO: Add anti-windup
 
         if self._derivative_on_process_value:
-            delta_output -= t_d / dt * (process_value[-1, :] - 2 * process_value[-2, :] + process_value[-3, :])
+            delta_output -= (t_d_matrix / dt) @ (process_value[:, -1] - 2 * process_value[:, -2] + process_value[:, -3])
         else:
-            delta_output += t_d / dt * (error[-1, :] - 2 * error[-2, :] + error[-3, :])
+            delta_output += (t_d_matrix / dt) @ (error[:, -1] - 2 * error[:, -2] + error[:, -3])
 
         # TODO: Add anti-windup
-        parameters = ca.vertcat(set_point[:], k_p, t_i, t_d)
-        self._function = ca.Function('function',
-                                     [process_value, ca.vertcat(output, parameters)],
-                                     [error, output + k_p * delta_output],
-                                     ['x0', 'p'],
-                                     ['error', 'yf'])
+        parameters = ca.vertcat(set_point.reshape((3 * self._n_set_points, -1)), k_p, t_i, t_d)
+        self._function = ca.Function(
+            'function',
+            [process_value.reshape((3 * self._n_set_points, -1)), ca.vertcat(output, parameters)],
+            [error, output + k_p_matrix @ delta_output],
+            ['x0', 'p'],
+            ['error', 'yf']
+        )
 
         self.check_consistency()
 
@@ -336,9 +350,9 @@ class PID(Controller, Base):
 
         pv = kwargs.get('pv')
         if pv is not None:
-            pv = convert(pv, ca.DM, axis=1)
+            pv = convert(pv, ca.DM)
         else:
-            pv = ca.DM.zeros(1, self._n_inputs)
+            pv = ca.DM.zeros(self._n_set_points, 1)
         self._append_process_value(pv)
         self._append_set_point()
         if self._solution.get_by_id('u').is_empty():
