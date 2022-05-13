9304fbfbbf3365da7f949db2c461c4563505f02a
Bruno <br.morabito@gmail.com>
2022-05-13 17:14:42 +0200

    add test SMPC

--- tests/test_SMPC.py
+++ tests/test_SMPC.py
@@ -1,8 +1,55 @@
 import unittest
+import casadi as ca
+import numpy as np
+from hilo_mpc import Model, SMPC, GP
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestIO(unittest.TestCase):
+    def setUp(self):
+        plant = Model(plot_backend='matplotlib')
+
+        states = plant.set_dynamical_states(['px'])
+        inputs = plant.set_inputs(['a'])
+        # parameter = plant.set_parameters(['p'])
+
+        # Unwrap states
+        px = states[0]
+
+        # Unwrap states
+        a = inputs[0]
+
+        # dpx = parameter*a
+        dpx =a
+
+        plant.set_dynamical_equations([dpx])
+
+        # Initial conditions
+        x0 = [15]
+
+        # Create plant and run simulation
+        dt = 1
+        plant.setup(dt=dt)
+        plant.set_initial_conditions(x0=x0)
+        model = plant.copy(setup=False)
+        model.discretize('erk', order=1, inplace=True)
+        model.setup(dt=dt)
+        model.set_initial_conditions(x0=x0)
+        self.model = model
+
+        # Setup GP
+        gp = GP(['px'], 'z', solver='ipopt')
+        X_train = np.array([[0., .5, 1. / np.sqrt(2.), np.sqrt(3.) / 2., 1., 0.]])
+        y_train = np.array([[0., np.pi / 6., np.pi / 4., np.pi / 3., np.pi / 2., np.pi]])
+        gp.set_training_data(X_train, y_train)
+        gp.setup()
+        gp.fit_model()
+        self.gp = gp
+        # Matrix
+        self.B = np.array([[1]])
+
+    def test_io_1(self):
+        smpc = SMPC(self.model, self.gp, self.B)
+
 
 if __name__ == '__main__':
     unittest.main()
