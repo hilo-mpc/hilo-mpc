7793b4399ef8a859cfe0959642113b1092c28efa
Johannes <johannes.pohlodek@ovgu.de>
2022-05-19 13:15:20 +0200

    Update tests/test_kernels.py
    Added tests for checking Matern32Kernel class

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -1137,3 +1137,488 @@ class TestExponentialKernel(TestCase):
         np.testing.assert_allclose(cov, np.array([[.28004858, .24311673, .21105534],
                                                   [.32259073, .28004858, .24311673],
                                                   [.37159546, .32259073, .28004858]]))
+
+
+class TestMatern32Kernel(TestCase):
+    """"""
+    def test_matern_32_kernel_no_hyperprior(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        self.assertIsNone(kernel.active_dims)
+        self.assertEqual(len(kernel.hyperparameters), 2)
+        self.assertEqual(kernel.hyperparameter_names, ['M32.length_scales', 'M32.signal_variance'])
+        self.assertTrue(hasattr(kernel.length_scales, 'log'))
+        np.testing.assert_equal(kernel.length_scales.value, np.ones((1, 1)))
+        np.testing.assert_equal(kernel.length_scales.log, np.zeros((1, 1)))
+        self.assertTrue(hasattr(kernel.signal_variance, 'log'))
+        np.testing.assert_equal(kernel.signal_variance.value, np.ones((1, 1)))
+        np.testing.assert_equal(kernel.signal_variance.log, np.zeros((1, 1)))
+
+    def test_matern_32_kernel_fixed(self) -> None:
+        """
+
+        :return:
+        """
+        # TODO: Change according to test_means.py when first TODO is finished
+        kernel = Kernel.matern_32()
+        kernel.length_scales.fixed = True
+        kernel.signal_variance.fixed = True
+
+        self.assertTrue(kernel.length_scales.fixed)
+        self.assertTrue(kernel.signal_variance.fixed)
+
+    # def test_matern_32_kernel_hyperprior_gaussian(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     # TODO: Create according to test_means.py when first TODO is finished
+
+    def test_matern_32_kernel_ard_no_active_dims(self) -> None:
+        """
+
+        :return:
+        """
+        with self.assertRaises(ValueError) as context:
+            Kernel.matern_32(ard=True)
+        self.assertEqual(str(context.exception),
+                         "The key word 'ard' can only be set to True if the key word 'active_dims' was supplied")
+
+    def test_matern_32_kernel_ard_dimension_mismatch(self) -> None:
+        """
+
+        :return:
+        """
+        with self.assertRaises(ValueError) as context:
+            Kernel.matern_32(active_dims=[0, 1], length_scales=[1., 1., 1.])
+        self.assertEqual(str(context.exception),
+                         "Dimension mismatch between 'active_dims' (2) and the number of length_scales (3)")
+
+    def test_matern_32_kernel_symbolic_call_sx(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = ca.SX.sym('x')
+        cov = kernel(x)
+
+        self.assertIsInstance(cov, ca.SX)
+        self.assertFalse(ca.depends_on(cov, kernel.length_scales.SX))
+        self.assertTrue(ca.depends_on(cov, kernel.signal_variance.SX))
+        self.assertFalse(ca.depends_on(cov, x))
+
+    # def test_matern_32_kernel_symbolic_call_mx(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     kernel = Kernel.matern_32()
+    #
+    #     x = ca.MX.sym('x')
+    #     # FIXME: This will result in a mixture of SX and MX (should we remove MX completely?)
+    #     cov = kernel(x)
+    #
+    #     self.assertIsInstance(cov, ca.MX)
+    #     self.assertFalse(ca.depends_on(cov, kernel.length_scales.MX))
+    #     self.assertTrue(ca.depends_on(cov, kernel.signal_variance.MX))
+    #     self.assertFalse(ca.depends_on(cov, x))
+
+    def test_matern_32_kernel_numeric_call(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = np.array([[1., 2., 3., 4., 5.]])
+        cov = kernel(x)
+
+        self.assertIsInstance(cov, np.ndarray)
+        np.testing.assert_allclose(cov, np.array([[1., .48335772, .13973135, .03431324, .007767734],
+                                                  [.48335772, 1., .48335772, .13973135, .03431324],
+                                                  [.13973135, .48335772, 1., .48335772, .13973135],
+                                                  [.03431324, .13973135, .48335772, 1., .48335772],
+                                                  [.007767734, .03431324, .13973135, .48335772, 1.]]))
+
+    def test_matern_32_kernel_symbolic_call_x_x_bar_wrong_type(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = ca.SX.sym('x')
+        y = np.array([[2.]])
+        # FIXME: Convert to TypeError
+        with self.assertRaises(ValueError) as context:
+            kernel(x, y)
+        self.assertEqual(str(context.exception), "X and X_bar need to have the same type")
+
+    def test_matern_32_kernel_symbolic_call_x_x_bar_dimension_mismatch(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = ca.SX.sym('x', 2)
+        y = ca.SX.sym('y')
+        # FIXME: Convert to ValueError
+        with self.assertRaises(AssertionError) as context:
+            kernel(x, y)
+        self.assertEqual(str(context.exception), "X and X_bar do not have the same input space dimensions")
+
+    def test_matern_32_kernel_symbolic_call_x_x_bar_sx(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = ca.SX.sym('x')
+        y = ca.SX.sym('y')
+        cov = kernel(x, y)
+
+        self.assertIsInstance(cov, ca.SX)
+        self.assertTrue(ca.depends_on(cov, kernel.length_scales.SX))
+        self.assertTrue(ca.depends_on(cov, kernel.signal_variance.SX))
+        self.assertTrue(ca.depends_on(cov, x))
+        self.assertTrue(ca.depends_on(cov, y))
+
+    # def test_matern_32_kernel_symbolic_call_x_x_bar_mx(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     kernel = Kernel.matern_32()
+    #
+    #     x = ca.MX.sym('x')
+    #     y = ca.MX.sym('y')
+    #     # FIXME: This will result in a mixture of SX and MX (should we remove MX completely?)
+    #     cov = kernel(x, y)
+    #
+    #     self.assertIsInstance(cov, ca.MX)
+    #     self.assertTrue(ca.depends_on(cov, kernel.length_scales.MX))
+    #     self.assertTrue(ca.depends_on(cov, kernel.signal_variance.MX))
+    #     self.assertTrue(ca.depends_on(cov, x))
+    #     self.assertTrue(ca.depends_on(cov, y))
+
+    def test_matern_32_kernel_numeric_call_x_x_bar_wrong_type(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = np.array([[2.]])
+        y = ca.SX.sym('y')
+        # FIXME: Convert to TypeError
+        with self.assertRaises(ValueError) as context:
+            kernel(x, y)
+        self.assertEqual(str(context.exception), "X and X_bar need to have the same type")
+
+    def test_matern_32_kernel_numeric_call_x_x_bar_dimension_mismatch(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = np.array([[1.], [2.]])
+        y = np.array([[1.]])
+        # FIXME: Convert to ValueError
+        with self.assertRaises(AssertionError) as context:
+            kernel(x, y)
+        self.assertEqual(str(context.exception), "X and X_bar do not have the same input space dimensions")
+
+    def test_matern_32_kernel_numeric_call_x_x_bar(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32()
+
+        x = np.array([[1., 2., 3., 4., 5.]])
+        y = np.array([[1., 2., 3., 4., 5.]]) / 2.
+        cov = kernel(x, y)
+
+        self.assertIsInstance(cov, np.ndarray)
+        np.testing.assert_allclose(
+            cov,
+            np.array([[.78488765, 1., .78488765, .48335772, .26775661],
+                      [.26775661, .48335772, .78488765, 1., .78488765],
+                      [.07017579, .13973135, .26775661, .48335772, .78488765],
+                      [.01645009, .03431324, .07017579, .13973135, .26775661],
+                      [.003624159, .007767734, .01645009, .03431324, .07017579]])
+                                   )
+
+    def test_matern_32_kernel_ard(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(active_dims=[0, 1], length_scales=[1., 1.])
+
+        self.assertEqual(kernel.active_dims, [0, 1])
+        np.testing.assert_equal(kernel.length_scales.value, np.ones((2, 1)))
+
+        kernel = Kernel.matern_32(active_dims=[0, 1, 2], ard=True)
+
+        self.assertEqual(kernel.active_dims, [0, 1, 2])
+        np.testing.assert_equal(kernel.length_scales.value, np.ones((3, 1)))
+
+    # def test_matern_32_kernel_ard_call_dimension_mismatch(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     # TODO: Create similar test for means
+    #     kernel = Kernel.matern_32(active_dims=[0, 1], length_scales=[1., 1.])
+    #
+    #     x = ca.SX.sym('x')
+    #     # FIXME: This will result in another error that could be unclear to the user. We should probably catch it and
+    #     #  return a more informative error message.
+    #     cov = kernel(x)
+
+    def test_matern_32_kernel_ard_call_dimension_mismatch(self) -> None:
+        """
+
+        :return:
+        """
+        # TODO: Create similar test for means
+        kernel = Kernel.matern_32(length_scales=[1., 1.])
+
+        x = ca.SX.sym('x')
+        with self.assertRaises(ValueError) as context:
+            kernel(x)
+        self.assertEqual(str(context.exception), "Length scales vector dimension does not equal input space dimension.")
+
+    def test_matern_32_kernel_ard_symbolic_call_sx(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(length_scales=[1., 1.])
+
+        x = ca.SX.sym('x', 2)
+        cov = kernel(x)
+
+        self.assertIsInstance(cov, ca.SX)
+        self.assertFalse(ca.depends_on(cov, kernel.length_scales.SX))
+        self.assertTrue(ca.depends_on(cov, kernel.signal_variance.SX))
+        self.assertFalse(ca.depends_on(cov, x))
+
+    def test_matern_32_kernel_ard_symbolic_call_sx_not_all_active(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(active_dims=[0, 2], length_scales=[1., 1.])
+
+        x = ca.SX.sym('x', 3)
+        cov = kernel(x)
+
+        self.assertIsInstance(cov, ca.SX)
+        self.assertFalse(ca.depends_on(cov, kernel.length_scales.SX))
+        self.assertTrue(ca.depends_on(cov, kernel.signal_variance.SX))
+        self.assertFalse(ca.depends_on(cov, x))
+
+    # def test_matern_32_kernel_ard_symbolic_call_mx(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     kernel = Kernel.matern_32(length_scales=[1., 1.])
+    #
+    #     x = ca.MX.sym('x', 2)
+    #     # FIXME: This will result in a mixture of SX and MX (should we remove MX completely?)
+    #     cov = kernel(x)
+    #
+    #     self.assertIsInstance(cov, ca.MX)
+    #     self.assertFalse(ca.depends_on(cov, kernel.length_scales.MX))
+    #     self.assertTrue(ca.depends_on(cov, kernel.signal_variance.MX))
+    #     self.assertFalse(ca.depends_on(cov, x))
+
+    # def test_matern_32_kernel_ard_symbolic_call_mx_not_all_active(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     kernel = Kernel.matern_32(active_dims=[0, 2], length_scales=[1., 1.])
+    #
+    #     x = ca.MX.sym('x', 3)
+    #     # FIXME: This will result in a mixture of SX and MX (should we remove MX completely?)
+    #     cov = kernel(x)
+    #
+    #     self.assertIsInstance(cov, ca.MX)
+    #     self.assertFalse(ca.depends_on(cov, kernel.length_scales.MX))
+    #     self.assertTrue(ca.depends_on(cov, kernel.signal_variance.MX))
+    #     self.assertFalse(ca.depends_on(cov, x))
+
+    def test_matern_32_kernel_ard_numeric_call(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(length_scales=[1., 1.])
+
+        x = np.array([[1., 2., 3., 4., 5.], [.1, .2, .3, .4, .5]])
+        cov = kernel(x)
+
+        self.assertIsInstance(cov, np.ndarray)
+        np.testing.assert_allclose(cov, np.array([[1., .48071535, .13786943, .03357525, .007536611],
+                                                  [.48071535, 1., .48071535, .13786943, .03357525],
+                                                  [.13786943, .48071535, 1., .48071535, .13786943],
+                                                  [.03357525, .13786943, .48071535, 1., .48071535],
+                                                  [.007536611, .03357525, .13786943, .48071535, 1.]
+                                                  ]))
+
+    def test_matern_32_kernel_ard_numeric_call_not_all_active(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(active_dims=[0, 2], length_scales=[1., 1.])
+
+        x = np.array([[1., 2., 3., 4., 5.], [6., 7., 8., 9., 0.], [.1, .2, .3, .4, .5]])
+        cov = kernel(x)
+
+        self.assertIsInstance(cov, np.ndarray)
+        np.testing.assert_allclose(cov, np.array([[1., .48071535, .13786943, .03357525, .007536611],
+                                                  [.48071535, 1., .48071535, .13786943, .03357525],
+                                                  [.13786943, .48071535, 1., .48071535, .13786943],
+                                                  [.03357525, .13786943, .48071535, 1., .48071535],
+                                                  [.007536611, .03357525, .13786943, .48071535, 1.]
+                                                  ]))
+
+    def test_matern_32_kernel_ard_symbolic_call_sx_x_x_bar(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(length_scales=[1., 1.])
+
+        x = ca.SX.sym('x', 2)
+        y = ca.SX.sym('y', 2)
+        cov = kernel(x, y)
+
+        self.assertIsInstance(cov, ca.SX)
+        for length_scale in kernel.length_scales.SX.elements():
+            self.assertTrue(ca.depends_on(cov, length_scale))
+        self.assertTrue(ca.depends_on(cov, kernel.signal_variance.SX))
+        for xk in x.elements():
+            self.assertTrue(ca.depends_on(cov, xk))
+        for yk in y.elements():
+            self.assertTrue(ca.depends_on(cov, yk))
+
+    def test_matern_32_kernel_ard_symbolic_call_sx_x_x_bar_not_all_active(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(active_dims=[0, 2], length_scales=[1., 1.])
+
+        x = ca.SX.sym('x', 3)
+        y = ca.SX.sym('y', 3)
+        cov = kernel(x, y)
+
+        self.assertIsInstance(cov, ca.SX)
+        for length_scale in kernel.length_scales.SX.elements():
+            self.assertTrue(ca.depends_on(cov, length_scale))
+        self.assertTrue(ca.depends_on(cov, kernel.signal_variance.SX))
+        for k, xk in enumerate(x.elements()):
+            if k == 1:
+                self.assertFalse(ca.depends_on(cov, xk))
+            else:
+                self.assertTrue(ca.depends_on(cov, xk))
+        for k, yk in enumerate(y.elements()):
+            if k == 1:
+                self.assertFalse(ca.depends_on(cov, yk))
+            else:
+                self.assertTrue(ca.depends_on(cov, yk))
+
+    # def test_matern_32_kernel_ard_symbolic_call_mx_x_x_bar(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     kernel = Kernel.matern_32(length_scales=[1., 1.])
+    #
+    #     x = ca.MX.sym('x', 2)
+    #     y = ca.MX.sym('y', 2)
+    #     # FIXME: This will result in a mixture of SX and MX (should we remove MX completely?)
+    #     cov = kernel(x, y)
+    #
+    #     self.assertIsInstance(cov, ca.MX)
+    #     for length_scale in kernel.length_scales.MX.elements():
+    #         self.assertTrue(ca.depends_on(cov, length_scale))
+    #     self.assertTrue(ca.depends_on(cov, kernel.signal_variance.MX))
+    #     for xk in x.elements():
+    #         self.assertTrue(ca.depends_on(cov, xk))
+    #     for yk in y.elements():
+    #         self.assertTrue(ca.depends_on(cov, yk))
+
+    # def test_matern_32_kernel_ard_symbolic_call_mx_x_x_bar_not_all_active(self) -> None:
+    #     """
+    #
+    #     :return:
+    #     """
+    #     kernel = Kernel.matern_32(active_dims=[0, 2], length_scales=[1., 1.])
+    #
+    #     x = ca.MX.sym('x', 3)
+    #     y = ca.MX.sym('y', 3)
+    #     # FIXME: This will result in a mixture of SX and MX (should we remove MX completely?)
+    #     cov = kernel(x, y)
+    #
+    #     self.assertIsInstance(cov, ca.MX)
+    #     for length_scale in kernel.length_scales.MX.elements():
+    #         self.assertTrue(ca.depends_on(cov, length_scale))
+    #     self.assertTrue(ca.depends_on(cov, kernel.signal_variance.MX))
+    #     for k, xk in enumerate(x.elements()):
+    #         if k == 1:
+    #             self.assertFalse(ca.depends_on(cov, xk))
+    #         else:
+    #             self.assertTrue(ca.depends_on(cov, xk))
+    #     for k, yk in enumerate(y.elements()):
+    #         if k == 1:
+    #             self.assertFalse(ca.depends_on(cov, yk))
+    #         else:
+    #             self.assertTrue(ca.depends_on(cov, yk))
+
+    def test_matern_32_kernel_ard_numeric_call_x_x_bar(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(length_scales=[1., 1.])
+
+        x = np.array([[1., 1.1, 1.2], [1.3, 1.4, 1.5]])
+        y = np.array([[1.6, 1.7, 1.8], [1.9, 2., 2.1]])
+        cov = kernel(x, y)
+
+        self.assertIsInstance(cov, np.ndarray)
+        np.testing.assert_allclose(cov, np.array([[.56801943, .48871175, .41705364],
+                                                  [.65370269, .56801943, .48871175],
+                                                  [.74319105, .65370269, .56801943]]))
+
+    def test_matern_32_kernel_ard_numeric_call_x_x_bar_not_all_active(self) -> None:
+        """
+
+        :return:
+        """
+        kernel = Kernel.matern_32(active_dims=[0, 2], length_scales=[1., 1.])
+
+        x = np.array([[1., 1.1, 1.2], [1.3, 1.4, 1.5], [1.6, 1.7, 1.8]])
+        y = np.array([[1.9, 2., 2.1], [2.2, 2.3, 2.4], [2.5, 2.6, 2.7]])
+        cov = kernel(x, y)
+
+        self.assertIsInstance(cov, np.ndarray)
+        np.testing.assert_allclose(cov, np.array([[.35346459, .29782077, .24967129],
+                                                  [.41705364, .35346459, .29782077],
+                                                  [.48871175, .41705364, .35346459]]))
