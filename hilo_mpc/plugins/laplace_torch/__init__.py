7e25cc4d7aa5155e30e674016c3f11f4fc5b5009
Johannes <johannes.pohlodek@ovgu.de>
2022-09-08 19:59:41 +0200

    Added hilo_mpc/plugins/laplace_torch/*
    Added wrapper for laplace-torch, a library that enables Laplace approximations of neural networks.

---
+++ hilo_mpc/plugins/laplace_torch/__init__.py
@@ -0,0 +1,41 @@
+#
+#   This file is part of HILO-MPC
+#
+#   HILO-MPC is toolbox for easy, flexible and fast development of machine-learning supported
+#   optimal control and estimation problems
+#
+#   Copyright (c) 2021 Johannes Pohlodek, Bruno Morabito, Rofl Findeisen
+#                      All rights reserved
+#
+#   HILO-MPC is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as
+#   published by the Free Software Foundation, either version 3
+#   of the License, or (at your option) any later version.
+#
+#   HILO-MPC is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with HILO-MPC. If not, see <http://www.gnu.org/licenses/>.
+
+from ..plugins import LearningManager
+
+from .wrapper import *
+
+
+def setup(kind, *args, **kwargs):
+    """
+
+    :param kind:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    net = LearningManager('pytorch').setup(kind, *args, **kwargs)
+    wrapper_obj = get_wrapper(net, **kwargs)
+    return wrapper_obj
+
+
+__all__ = []
