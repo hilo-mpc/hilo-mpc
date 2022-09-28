831342f4aa769a014a97765b3186fdc17307f28a
pohlodek <johannes.pohlodek@ovgu.de>
2022-09-28 15:03:30 +0200

    Update hilo_mpc/util/probability.py
    Updated distribution classes

--- hilo_mpc/util/probability.py
+++ hilo_mpc/util/probability.py
@@ -37,18 +37,30 @@ class Distribution(metaclass=ABCMeta):
     """"""
     def __init__(self):
         """Constructor method"""
-        self._function = None
-        self._log_function = None
+        self._pdf_function = None
+        self._log_pdf_function = None
+        self._cdf_function = None
         self._initialize()
 
     def __call__(self, *args, **kwargs) -> Union[ca.SX, ca.MX, ca.DM]:
         """Calling method"""
+        which = kwargs.get('which')
+        if which is None:
+            which = 'pdf'
+
         log = kwargs.get('log')
         if log is None:
             log = False
 
-        if log:
-            return self._log_function(*args)
+        if which == 'pdf':
+            if log:
+                return self.probability_density_function(*args, log=True)
+            else:
+                return self.probability_density_function(*args, log=False)
+        elif which == 'cdf':
+            return self.cumulative_distribution_function(*args)
+        else:
+            raise ValueError(f"Key word 'which={which}' not recognized")
 
     @abstractmethod
     def _initialize(self) -> None:
@@ -57,6 +69,30 @@ class Distribution(metaclass=ABCMeta):
         :return:
         """
         pass
+
+    def probability_density_function(self, *args, log: bool = False) -> Union[ca.SX, ca.MX, ca.DM]:
+        """
+
+        :param args:
+        :param log:
+        :return:
+        """
+        if log:
+            return self._log_pdf_function(*args)
+        else:
+            return self._pdf_function(*args)
+
+    pdf = probability_density_function
+
+    def cumulative_distribution_function(self, *args) -> Union[ca.SX, ca.MX, ca.DM]:
+        """
+
+        :param args:
+        :return:
+        """
+        return self._cdf_function(*args)
+
+    cdf = cumulative_distribution_function
 
 
 class Gaussian(Distribution):
@@ -71,11 +107,13 @@ class Gaussian(Distribution):
         mu = ca.SX.sym('mu')
         var = ca.SX.sym('var')
 
+        pdf = 1. / ca.sqrt(2. * ca.pi * var) * ca.exp(-.5 * (y - mu) ** 2. / var)
         log_pdf = -(y - mu) ** 2. / (2. * var) - ca.log(2. * ca.pi * var) / 2.
+        cdf = (1. + ca.erf((y - mu) / ca.sqrt(2. * var))) / 2.
 
-        self._log_function = ca.Function('log_pdf',
-                                         [y, mu, var],
-                                         [log_pdf])
+        self._pdf_function = ca.Function('pdf', [y, mu, var], [pdf])
+        self._log_pdf_function = ca.Function('log_pdf', [y, mu, var], [log_pdf])
+        self._cdf_function = ca.Function('cdf', [y, mu, var], [cdf])
 
 
 class Laplace(Distribution):
@@ -91,11 +129,13 @@ class Laplace(Distribution):
         var = ca.SX.sym('var')
 
         b = ca.sqrt(var / 2.)
+        pdf = ca.exp(-ca.fabs(y - mu) / b) / (2. * b)
         log_pdf = -ca.fabs(y - mu) / b - ca.log(2. * b)
+        cdf = .5 + .5 * ca.sign(y - mu) * (1. - ca.exp(-ca.fabs(y - mu) / b))
 
-        self._log_function = ca.Function('log_pdf',
-                                         [y, mu, var],
-                                         [log_pdf])
+        self._pdf_function = ca.Function('pdf', [y, mu, var], [pdf])
+        self._log_pdf_function = ca.Function('log_pdf', [y, mu, var], [log_pdf])
+        self._cdf_function = ca.Function('cdf', [y, mu, var], [cdf])
 
 
 class StudentsT(Distribution):
@@ -110,13 +150,35 @@ class StudentsT(Distribution):
         mu = ca.SX.sym('mu')
         var = ca.SX.sym('var')
         nu = ca.SX.sym('nu')
+        gamma_1 = ca.SX.sym('gamma_1')
+        gamma_2 = ca.SX.sym('gamma_2')
+        gammaln_1 = ca.SX.sym('gammaln_1')
+        gammaln_2 = ca.SX.sym('gammaln_2')
 
-        log_Z = gammaln((nu + 1.) / 2.) - gammaln(nu / 2.) - ca.log(var * (nu - 2.) * ca.pi) / 2.
+        Z = gamma_1 / (ca.sqrt(var * (nu - 2.) * ca.pi) * gamma_2)
+        pdf = Z * (1. + (y - mu) ** 2. / (var * (nu - 2.)))
+        log_Z = gammaln_1 - gammaln_2 - ca.log(var * (nu - 2.) * ca.pi) / 2.
         log_pdf = log_Z - (nu + 1.) / 2. * ca.log(1. + (y - mu) ** 2. / (var * (nu - 2.)))
 
-        self._log_function = ca.Function('log_pdf',
-                                         [y, mu, var, nu],
-                                         [log_pdf])
+        self._pdf_function = ca.Function('pdf', [y, mu, var, nu, gamma_1, gamma_2], [pdf])
+        self._log_pdf_function = ca.Function('log_pdf', [y, mu, var, nu, gammaln_1, gammaln_2], [log_pdf])
+
+    def probability_density_function(self, *args, log: bool = False) -> Union[ca.SX, ca.MX, ca.DM]:
+        """
+
+        :param args:
+        :param log:
+        :return:
+        """
+        nu = args[-1]
+        if log:
+            gammaln_1 = gammaln((nu + 1.) / 2.)
+            gammaln_2 = gammaln(nu / 2.)
+            return self._log_pdf_function(*args, gammaln_1, gammaln_2)
+        else:
+            gamma_1 = gamma((nu + 1.) / 2.)
+            gamma_2 = gamma(nu / 2.)
+            return self._pdf_function(*args, gamma_1, gamma_2)
 
 
 class Gamma(Distribution):
@@ -132,7 +194,7 @@ class Gamma(Distribution):
 
         pdf = beta ** alpha / gamma(alpha) * y ** (alpha - 1) * ca.exp(-beta * y)
 
-        self._function = ca.Function('pdf', [y, alpha, beta], [pdf])
+        self._pdf_function = ca.Function('pdf', [y, alpha, beta], [pdf])
 
 
 class Prior(metaclass=ABCMeta):
@@ -140,13 +202,13 @@ class Prior(metaclass=ABCMeta):
     def __init__(self, name: str) -> None:
         """Constructor method"""
         self._name = name
-        self._pdf = None
+        self._distribution = None
 
     def __call__(self, *args, **kwargs) -> Union[ca.SX, ca.MX, ca.DM]:
         """Calling method"""
         x = args[0]
         p = self._get_parameter_values()
-        return self._pdf(x, *p, **kwargs)
+        return self._distribution(x, *p, **kwargs)
 
     @staticmethod
     def _check_dimensionality(parameter: Any, name: str) -> Numeric:
@@ -244,6 +306,29 @@ class Prior(metaclass=ABCMeta):
         :return:
         """
         return GammaPrior(shape=shape, rate=rate, scale=scale)
+
+    def probability_density_function(self, *args, log: bool = False) -> Union[ca.SX, ca.MX, ca.DM]:
+        """
+
+        :param args:
+        :param log:
+        :return:
+        """
+        p = self._get_parameter_values()
+        return self._distribution.probability_density_function(*args, *p, log=log)
+
+    pdf = probability_density_function
+
+    def cumulative_distribution_function(self, *args) -> Union[ca.SX, ca.MX, ca.DM]:
+        """
+
+        :param args:
+        :return:
+        """
+        p = self._get_parameter_values()
+        return self._distribution.cumulative_distribution_function(*args, *p)
+
+    cdf = cumulative_distribution_function
 
 
 class _MeanVariancePrior(Prior):
